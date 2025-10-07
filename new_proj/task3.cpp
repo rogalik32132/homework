a8ce397efc881de64bd2e65c10c2acce595631fa
rogalik32132 <nikita.shvedovskiy@gmail.com>
2025-10-07 21:16:37 +0000

    Task3

---
+++ new_proj/task3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main() {
+	const char* text = "Hello Ukraine";
+	int length = strlen(text);
+
+	cout << "Text: " << text << endl;
+	cout << "Length: " << length << " characters" << endl;
+
+	return 0;
+ }

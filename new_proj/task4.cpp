03e2b853121152ae0468050a64084bfceb5f9675
rogalik32132 <nikita.shvedovskiy@gmail.com>
2025-10-07 21:25:58 +0000

    Task4

--- new_proj/task4.cpp
+++ new_proj/task4.cpp
@@ -1,27 +1,14 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
-int sumOfDigits(int n) {
-	int sum = 0;
-	while (n > 0) {
-		sum += n % 10;
-		n /=10;
-	}
-	return sum;
-}
-
 int main() {
-	int count = 0;
-
-	for (int ticket = 0; ticket <= 999999; ticket++) {
-		int firstHalf = ticket /1000;
-		int secondHalf = ticket % 1000;
-
-		if(sumOfDigits(firstHalf) == sumOfDigits(secondHalf)) {
-			count++;
-		}
-	}
-
-	cout << "Total lucky tickets: " << count << endl;
-	return 0;
+    int number;
+    cout << "Введите число: ";
+    cin >> number;
+    
+    string str = to_string(number);
+    cout << "Строка: " << str << endl;
+    
+    return 0;
 }

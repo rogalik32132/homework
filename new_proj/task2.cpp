462160e6a5402b8d0641a497b34356ecb73f13d2
rogalik32132 <nikita.shvedovskiy@gmail.com>
2025-10-07 21:14:23 +0000

    Task2

---
+++ new_proj/task2.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+
+
+using namespace std;
+
+
+
+
+
+int main() {
+
+
+	int number;
+
+
+	cout << "Enter number: ";
+
+
+	cin >> number;
+
+
+
+
+
+	if (number > 0) {
+
+
+	    cout << "Positive number" << endl;
+
+
+	} else if (number < 0) {
+
+
+	    cout << "Negative number" << endl;
+
+
+	} else {
+
+
+	    cout << "Zero" << endl;
+
+
+	}
+
+
+
+
+
+	return 0;
+
+
+   }
+
+
